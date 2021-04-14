90c1541764891c891d74e81e40f9156793303796
makhatadze <vitomaxatadze@gmail.com>
2021-04-14 11:20:46 +0400

    Import express and run server.

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,18 @@
 import {MikroORM} from "@mikro-orm/core";
-import {Post} from "./entities/Post";
 import microConfig from './mikro-orm.config';
-
+import express from 'express';
 const main = async () => {
     const orm = await MikroORM.init(microConfig);
-    // const post = orm.em.create(Post, {title: 'my first post'})
+    await orm.getMigrator().up();
+    const app = express();
 
-    // await orm.em.persistAndFlush(post);
+    app.get('/', (_, res) => {
+        res.send('Hello')
+    });
 
-    const posts = await orm.em.find(Post, {});
-    console.log(posts);
+    app.listen(4000, () => {
+        console.log('server started on localhost:4000')
+    })
 }
 
 main().catch(err => {

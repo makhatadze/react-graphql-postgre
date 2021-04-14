676a01e175af1ec967c2750ff271e424b4715468
makhatadze <vitomaxatadze@gmail.com>
2021-04-14 13:44:15 +0400

    Add register mutation.

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import {ApolloServer} from 'apollo-server-express';
 import {buildSchema} from 'type-graphql';
 import {HelloResolver} from "./resolvers/hello";
 import {PostResolver} from "./resolvers/post";
+import {UserResolver} from "./resolvers/user";
 const main = async () => {
     const orm = await MikroORM.init(microConfig);
     await orm.getMigrator().up();
@@ -13,7 +14,7 @@ const main = async () => {
 
     const apolloServer = new ApolloServer({
         schema: await buildSchema({
-            resolvers: [HelloResolver, PostResolver],
+            resolvers: [HelloResolver, PostResolver, UserResolver],
             validate: false
         }),
         context: () => ({ em: orm.em })

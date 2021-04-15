a397794fd07463da84174ff920a4ca8bc23abe4c
makhatadze <vitomaxatadze@gmail.com>
2021-04-15 10:43:32 +0400

    Install cors policy.

--- server/src/index.ts
+++ server/src/index.ts
@@ -12,7 +12,7 @@ import session from "express-session";
 import connectRedis from 'connect-redis';
 import {__prod__} from "./constants";
 import {MyContext} from "./types";
-
+import cors from 'cors';
 
 const main = async () => {
     const orm = await MikroORM.init(microConfig);
@@ -21,6 +21,11 @@ const main = async () => {
 
     const RedisStore = connectRedis(session)
     const redisClient = redis.createClient()
+
+    app.use(cors({
+        origin: "http://localhost:3000",
+        credentials: true
+    }));
 
     app.use(
         session({
@@ -49,7 +54,12 @@ const main = async () => {
         context: ({req,res}): MyContext => ({em: orm.em, req, res})
     })
 
-    apolloServer.applyMiddleware({app});
+    apolloServer.applyMiddleware({
+        app,
+        cors: {
+            origin: false
+        }
+    });
 
     app.listen(4000, () => {
         console.log('server started on localhost:4000')

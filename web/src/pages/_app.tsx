82580d8a3d9ee0f17fee1fd05b73247582d89b65
makhatadze <vitomaxatadze@gmail.com>
2021-04-15 14:09:25 +0400

    Cache update on login and register.

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,13 +1,62 @@
 import {ChakraProvider, ColorModeProvider} from '@chakra-ui/react'
 
 import theme from '../theme'
-import {createClient, Provider} from "urql";
+import {createClient, dedupExchange, fetchExchange, Provider} from "urql";
+import {cacheExchange, Cache, QueryInput} from "@urql/exchange-graphcache";
+import {LoginMutation, MeDocument, MeQuery, RegisterMutation} from "../generated/graphql";
+
+function betterUpdateQuery<Result, Query>(
+    cache: Cache,
+    qi: QueryInput,
+    result: any,
+    fn: (r: Result, q: Query) => Query
+) {
+    return cache.updateQuery(qi, data => fn(result, data as any) as any)
+}
 
 const client = createClient({
         url: 'http://localhost:4000/graphql',
         fetchOptions: {
             credentials: 'include'
-        }
+        },
+        exchanges: [dedupExchange, cacheExchange({
+            updates: {
+                Mutation: {
+                    login: (_result: LoginMutation, _, cache, __) => {
+                        betterUpdateQuery<LoginMutation, MeQuery>(cache, {
+                                query: MeDocument
+                            },
+                            _result,
+                            (result, query) => {
+                                if (result.login.errors) {
+                                    return query;
+                                } else {
+                                    return {
+                                        me: result.login.user
+                                    }
+                                }
+                            }
+                        )
+                    },
+                    register: (_result: LoginMutation, _, cache, __) => {
+                        betterUpdateQuery<RegisterMutation, MeQuery>(cache, {
+                                query: MeDocument
+                            },
+                            _result,
+                            (result, query) => {
+                                if (result.register.errors) {
+                                    return query;
+                                } else {
+                                    return {
+                                        me: result.register.user
+                                    }
+                                }
+                            }
+                        )
+                    }
+                }
+            }
+        }), fetchExchange]
     }
 )
 
